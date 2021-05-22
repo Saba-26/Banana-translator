1067d7465318965391013507543999aad921c10f
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-05-22 19:05:58 +0530

    styling is added

---
+++ app.js
@@ -0,0 +1,28 @@
+var transalte = document.querySelector("#button_translate");
+var textInput = document.querySelector("#text-input");
+var Output = document.querySelector("#output");
+
+var serverURL = " ";
+
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
+function clickHandler() {
+  var Text = textInput.value;
+  fetch(getTranslationURL(Text))
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      var translatedText = json.contents.translated;
+      Output.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+function errorHandler(error) {
+  console.log("Error Occured", error);
+  alert("something wrong with Server! Please try again after some time");
+}
+
+transalte.addEventListener("click", clickHandler);
